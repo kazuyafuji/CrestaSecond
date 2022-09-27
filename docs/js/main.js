6bc02a80d669ce7f0a72caca079277102453b0e5
kazuya fujiwara <kazuyafujiwara@kazuyanoMacBook-Pro.local>
2022-09-28 03:01:26 +0900

    smoothScroll headerのscrollTopでbg-colorchange

--- docs/js/main.js
+++ docs/js/main.js
@@ -2,3 +2,22 @@ $(".header-humberger-area, #humberger-nav-item-link").on("click", function () {
   $(".header-humberger").toggleClass("active");
   $(".humberger-modal").toggleClass("active");
 });
+
+$(function () {
+  $(window).scroll(function () {
+    var window_width = $(window).width();
+    if (window_width > 768) {
+      if ($(this).scrollTop() > 656) {
+        $(".header").addClass("header-bg-dark");
+      } else {
+        $(".header").removeClass("header-bg-dark");
+      }
+    } else {
+      if ($(this).scrollTop() > 504) {
+        $(".header").addClass("header-bg-dark");
+      } else {
+        $(".header").removeClass("header-bg-dark");
+      }
+    }
+  });
+});
